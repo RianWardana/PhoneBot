77549a1fc473acd2e852cbb744e5aff90171d8c2
Rian Wardana <rian@rianwardana.com>
2016-08-28 09:09:46 +0700

    media sync improvement over Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -7,11 +7,23 @@ gulp.task('build', function() {
 	.pipe(gulp.dest('../cordova/www'))
 });
 
+gulp.task('cloneMedia', function() {
+	return gulp.src('src/media/*.*')
+	.pipe(gulp.dest('../cordova/www/src/media'))
+});
+
+gulp.task('cloneImages', function() {
+	return gulp.src('img/*.*')
+	.pipe(gulp.dest('../cordova/www/img'))
+})
+
 gulp.task('watch', function() {
 	gulp.watch('bower_components/**/*.*', ['build']);
-	gulp.watch('src/**/*.*', ['build']);
+	gulp.watch('src/*.*', ['build']);
 	gulp.watch('index.html', ['build']);
 	gulp.watch('polymer.json', ['build']);
+	gulp.watch('src/media/*.*', ['cloneMedia']);
+	gulp.watch('img/*.*', ['cloneImages']);
 });
 
 gulp.task('default', ['build', 'watch']);
